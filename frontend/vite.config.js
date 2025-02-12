eb8427f02b6230f1a5e77f752e96f34606d71e76
davidajagbe <davidajagbe2004@gmail.com>
2025-02-13 00:31:52 +0100

    vite.config changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': process.env, // Include this line
+    'import.meta.env.VITE_BACKEND_BASEURL': JSON.stringify(process.env.VITE_BACKEND_BASEURL), // Include this line
+  },
   server: {
     port: 8080,
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_BACKEND_BASEURL, // Point to my backend
+        target: process.env.VITE_BACKEND_BASEURL, // Point to my backend
         changeOrigin: true,
         secure: false,
       },
