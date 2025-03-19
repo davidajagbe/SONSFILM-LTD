fde1f784e9456474cf63729c59d6cb1ed24b6d06
davidajagbe <davidajagbe2004@gmail.com>
2025-03-19 23:16:22 +0100

    changes sameSite to "lax"

--- backend/utils/CookieToken.js
+++ backend/utils/CookieToken.js
@@ -13,9 +13,9 @@ const CookieToken = (res, userId) => {
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development',//will use secure cookies in production
     maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
-    sameSite: 'strict',//prevents CRF attacks
+    sameSite: 'lax',//prevents CRF attacks
   });
-  
+
   console.log("Cookie Token Generated: ", token);
   console.log("Cookie Set: ", res.getHeaders());
 };
